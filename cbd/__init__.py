446abccffe51cf89cfbd6be18aa4e1698d625095
You-Sheng Yang <vicamo@gmail.com>
2025-04-23 18:07:33 +0800

    Init python project
    Signed-off-by: You-Sheng Yang <vicamo@gmail.com>

---
+++ cbd/__init__.py
@@ -0,0 +1,4 @@
+"""cbd top-level package."""
+
+__app_name__ = "cbd"
+__version__ = "0.0.1"
---
+++ cbd/__main__.py
@@ -0,0 +1,10 @@
+"""cbd entry point script."""
+
+
+def main():
+    """Main entry function."""
+    return
+
+
+if __name__ == "__main__":
+    main()
